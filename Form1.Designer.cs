c51ea8847aee8768e41d6182f0b70a39c7323de3
Krish Jansari <krish.jansari@edu.jonkoping.se>
2022-04-25 13:51:36 +0200

    Första Loggboks uppdateringen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,15 @@ namespace Slutprojekt_Krish_Jansari1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
